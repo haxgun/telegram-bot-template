3b45630d2371e620c49155b63606da528588ab3d
Misha Gusev <majikx.prod0@gmail.com>
2025-01-31 09:01:39 +0500

    chore(handlers): add new welcome message for start

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -1,11 +1,15 @@
 from aiogram import Router
-from aiogram.filters import Command
+from aiogram.filters import CommandStart
 from aiogram.types import Message
 from fluentogram import TranslatorRunner
 
 router = Router()
 
-@router.message(Command("start"))
-async def _(message: Message,
-            i18n: TranslatorRunner):
-    await message.answer(i18n.welcome_text())
+@router.message(CommandStart())
+async def _(message: Message, i18n: TranslatorRunner) -> None:
+    welcome_text = i18n.welcome_text()
+    repository_link = i18n.repository_link()
+    click_on_link = i18n.click_on_link()
+
+    text = f"{welcome_text}\n{repository_link}\n{click_on_link}"
+    await message.reply(text)
--- bot/i18n/ru/text.ftl
+++ bot/i18n/ru/text.ftl
@@ -1,1 +1,3 @@
-welcome_text=hi
+welcome_text = 👋 Привет! Это Telegram бот, доступный в репозитории:
+repository_link = 🔗 https://github.com/haxgun/telegram-bot-template
+click_on_link = 🔧 Загляни в репозиторий, чтобы узнать больше и начать использовать бота!
